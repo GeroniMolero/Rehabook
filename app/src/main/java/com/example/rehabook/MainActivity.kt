63f44b0434bfbe888024cc51c36acf5ee6ef0dec
GeroniMolero <gmolrod892@g.educaand.es>
2025-11-04 12:38:09 +0100

    Segundo commit, estructura de proyecto

--- app/src/main/java/com/example/rehabook/MainActivity.kt
+++ app/src/main/java/com/example/rehabook/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.rehabook.ui.theme.RehabookTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,4 +49,22 @@ fun GreetingPreview() {
     RehabookTheme {
         Greeting("Android")
     }
+}
+
+
+@Composable
+fun pantallaInicio(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.pajaroestesi),
+        contentDescription = "logo pajarito",
+        modifier = Modifier,
+        alignment= Alignment.Center,
+        contentScale = ContentScale.Fit,
+    )
+}
+
+@Preview
+@Composable
+fun pantallaInicioPreview(){
+    pantallaInicio()
 }
---
+++ app/src/main/java/com/example/rehabook/MyApp.java
@@ -0,0 +1,4 @@
+package com.example.rehabook;
+
+public class MyApp {
+}
